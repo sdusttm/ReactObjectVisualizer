dafe2c33da86b67d84a664219d2ed703b2afe173
Meng Tian <sdusttm@gmail.com>
2021-05-01 23:42:00 -0700

    start to work

--- app/config/webpack.config.js
+++ app/config/webpack.config.js
@@ -328,6 +328,8 @@ module.exports = function (webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
+        // make sure library won't reference another copy of React
+        react: path.resolve("./node_modules/react"),
         // Allows for better profiling with ReactDevTools
         ...(isEnvProductionProfile && {
           'react-dom$': 'react-dom/profiling',
