56c593cd18721858193c7d889c45bfd218f179c9
Meng Tian <sdusttm@gmail.com>
2021-05-02 16:40:57 -0700

    show link correctly

--- component.library/src/object-visualizer.tsx
+++ component.library/src/object-visualizer.tsx
@@ -32,10 +32,12 @@ export const ObjectVisualizer = ({ data, configs }: DataViewerProps) => {
     const ObjectSizeLimit = defaultExpandObjectSizeLimit || 0;
 
     const isArray = data && Object.keys(data).every((x) => !isNaN(x as any));
+    const isEmptyArray = isArray && Object.entries(data).length === 0;
     const isObjet = data !== null && typeof data === "object" && !isArray;
     const isSmallArrayOrObject = (isArray || isObjet) && JSON.stringify(data).length < ObjectSizeLimit;
     const items = data ? Object.entries(data).filter((x) => x[1] !== undefined) : [];
     const isStringArray = isArray && Object.values(data).every((x) => typeof x === "string");
+    const showLink = !!data && !isEmptyArray;
     const [expand, setExpand] = useState(expandLevels > 0 || isSmallArrayOrObject || isSmallArrayOrObject);
     return (
         <>
@@ -63,7 +65,7 @@ export const ObjectVisualizer = ({ data, configs }: DataViewerProps) => {
                     </table>
                 )}
             </div>
-            {!expand && !!data && (
+            {showLink && (
                 <div style={{ textAlign: "left" }}>
                     <a style={{ fontSize: 14 }} onClick={() => setExpand(!expand)}>
                         {expand ? "collapse" : "expand"}
