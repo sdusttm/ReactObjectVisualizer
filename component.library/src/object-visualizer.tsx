0ea829fa43c7cc5469b83cd49dda8447c38b70f9
Meng Tian <sdusttm@gmail.com>
2021-05-03 01:00:07 -0700

    tweaks

--- component.library/src/object-visualizer.tsx
+++ component.library/src/object-visualizer.tsx
@@ -16,36 +16,36 @@ const hide = {
     display: "none"
 } as any;
 
-interface DataViewerConfigs {
+interface ObjectVisualizerConfigs {
     defaultExpandLevels?: number;
     defaultExpandObjectSizeLimit?: number;
 }
 
-interface DataViewerProps {
-    data: any;
-    configs?: DataViewerConfigs;
+interface ObjectVisualizerProps {
+    object: any;
+    configs?: ObjectVisualizerConfigs;
 }
 
-export const ObjectVisualizer = ({ data, configs }: DataViewerProps) => {
+export const ObjectVisualizer = ({ object, configs }: ObjectVisualizerProps) => {
     const { defaultExpandLevels, defaultExpandObjectSizeLimit } = configs || {};
     const expandLevels = defaultExpandLevels === undefined ? 1 : defaultExpandLevels;
     const ObjectSizeLimit = defaultExpandObjectSizeLimit || 0;
 
-    const isArray = data && Object.keys(data).every((x) => !isNaN(x as any));
-    const isEmptyArray = isArray && Object.entries(data).length === 0;
-    const isStringArray = isArray && Object.values(data).every((x) => typeof x === "string");
-    const isNumberArray = isArray && Object.values(data).every((x) => typeof x === "number");
-    const isObjet = data !== null && typeof data === "object" && !isArray;
-    const isSmallArrayOrObject = (isArray || isObjet) && JSON.stringify(data).length < ObjectSizeLimit;
-    const items = data ? Object.entries(data).filter((x) => x[1] !== undefined) : [];
-    const showLink = !!data && !isEmptyArray;
+    const isArray = object && Object.keys(object).every((x) => !isNaN(x as any));
+    const isEmptyArray = isArray && Object.entries(object).length === 0;
+    const isStringArray = isArray && Object.values(object).every((x) => typeof x === "string");
+    const isNumberArray = isArray && Object.values(object).every((x) => typeof x === "number");
+    const isObjet = object !== null && typeof object === "object" && !isArray;
+    const isSmallArrayOrObject = (isArray || isObjet) && JSON.stringify(object).length < ObjectSizeLimit;
+    const items = object ? Object.entries(object).filter((x) => x[1] !== undefined) : [];
+    const showLink = !!object && !isEmptyArray;
     const [expand, setExpand] = useState(expandLevels > 0 || isSmallArrayOrObject);
 
     let collapsedContentType = "";
     if (isObjet) {
         collapsedContentType = "object";
     } else if (isArray) {
-        const length = Object.values(data).length;
+        const length = Object.values(object).length;
         collapsedContentType = `${isStringArray ? "string" : isNumberArray ? "number" : "object"}[${length}]`;
     }
 
@@ -59,12 +59,14 @@ export const ObjectVisualizer = ({ data, configs }: DataViewerProps) => {
                             <td style={cell}>
                                 {typeof item[1] === "object"
                                     ? ObjectVisualizer({
-                                          data: item[1],
+                                          object: item[1],
                                           configs: {
                                               ...configs,
-                                              defaultExpandLevels: isSmallArrayOrObject ? 1000 : expandLevels - 1, // if object is small, we just expand to all layers
+                                              defaultExpandLevels: isSmallArrayOrObject
+                                                  ? expandLevels
+                                                  : expandLevels - 1, // if object is small, we just keep expanding
                                               defaultExpandObjectSizeLimit:
-                                                  defaultExpandLevels > 0 ? defaultExpandObjectSizeLimit : 0 // we only want to apply defaultExpandObjectSizeLimit on top level
+                                                  defaultExpandLevels > 0 ? defaultExpandObjectSizeLimit : 0 // we only want to apply defaultExpandObjectSizeLimit on top levels
                                           }
                                       })
                                     : JSON.stringify(item[1])}
